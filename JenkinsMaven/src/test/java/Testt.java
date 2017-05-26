dd42baf494380d23112621374c75b41ff952fa81
Anil <Anil@AnilRaju>
2017-05-26 19:23:39 +0530

    1st commit

---
+++ JenkinsMaven/src/test/java/Testt.java
@@ -0,0 +1,20 @@
+
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Testt {
+
+	WebDriver driver;
+		@Test
+		public void launch(){
+			
+		System.out.println("Helooooooooooooooooooooo");
+		System.setProperty("webdriver.chrome.driver", "C:\\Automation Stuff\\chromedriver.exe");
+		driver=new ChromeDriver();
+		driver.get("https://www.google.com");
+	}
+
+}
